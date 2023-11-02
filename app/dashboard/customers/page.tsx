b104412b8bec04c823bcd49da9e8273a1358d666
getoarmorina3 <getoaarm1@gmail.com>
2023-11-02 13:18:03 +0100

    Add Customer page, filter, pagination

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,34 @@
+import { fetchFilteredCustomers, fetchCustomersPages } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+import Pagination from '@/app/ui/invoices/pagination';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-  return <p>Customers page</p>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const customers = await fetchFilteredCustomers(query, currentPage);
+  const totalPages = await fetchCustomersPages(query);
+
+  return (
+    <main>
+      <>
+        <CustomersTable customers={customers} />
+        <div className="mt-5 flex w-full justify-center">
+          <Pagination totalPages={totalPages} />
+        </div>
+      </>
+    </main>
+  );
 }
