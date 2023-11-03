94009163dce70c5240f32250591c772de47ec74c
getoarmorina3 <getoaarm1@gmail.com>
2023-11-03 13:32:20 +0100

    import web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
-import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts';
-import { Metadata } from 'next';
- 
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
+import { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
+
 export const metadata: Metadata = {
   title: {
-    template: '%s | Acme Dashboard',
-    default: 'Acme Dashboard',
+    template: "%s | Acme Dashboard",
+    default: "Acme Dashboard",
   },
-  description: 'The official Next.js Learn Dashboard built with App Router.',
+  description: "The official Next.js Learn Dashboard built with App Router.",
 };
 
 export default function RootLayout({
@@ -17,7 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
